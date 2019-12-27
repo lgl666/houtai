391ebd8ec15e31147a9408303d8d783da815ebf5
lgl666 <2622414362@qq.com>
2019-12-27 12:16:14 +0800

    之前的项目有问题 重新提交

---
+++ src/test/java/com/example/demo222/Demo222ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo222;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo222ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
